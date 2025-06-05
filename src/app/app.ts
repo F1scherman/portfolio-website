c745bc58e05e81806b15ff4a081bc862a1bb513c
F1scherman <brayden.f.jonsson@gmail.com>
2025-06-04 18:23:06 -0600

    Remove excess files and update the site

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected title = 'portfolio-site';
